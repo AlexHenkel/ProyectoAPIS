f3abefb7d10310729dabbb4a9cfd4162aab7c136
Alejandro Henkel <alehenkel17@gmail.com>
2017-10-25 12:09:22 -0500

    Integrate first component with new modals and forms

--- src/Containers/Dashboard/Groups.jsx
+++ src/Containers/Dashboard/Groups.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {
@@ -12,6 +12,8 @@ import { withTheme } from 'material-ui/styles'
 import AddIcon from 'material-ui-icons/Add'
 import { AlignCenter } from '../../Components/Utils'
 import ContextMenu, { ContextContainer } from '../../Components/ContextMenu'
+import ModalSave from '../../Components/ModalSave'
+import ModalRemove from '../../Components/ModalRemove'
 
 const Card = styled(OriginalCard)`
   margin: 15px 0;
@@ -26,50 +28,116 @@ const CardText = styled(Typography)`
 const DisabledButton = styled(Button)`
   color: rgba(255,255,255,0.75) !important;
 `
+class Groups extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      modalType: 'create',
+      modalSaveOpened: false,
+      modalRemoveOpened: false,
+      toModifyId: -1,
+    }
+  }
 
-const Groups = ({
-  theme,
-  groups,
-  selectActiveGroup,
-  activeGroup,
-}) => (
-  <div>
-    <Typography type="display1" gutterBottom>Mis cursos</Typography>
-    {groups.map(({ id, name, activeQuizes }) => (
-      <Card key={id} active={activeGroup === id ? 'yes' : ''} theme={theme}>
-        <CardContent>
-          <ContextContainer>
-            <ContextMenu
-              value={id}
-              handleEdit={id => console.log('edit', id)}
-              handleRemove={id => console.log('remove', id)}
-            />
-          </ContextContainer>
-          <CardText type="headline" component="h2" gutterBottom active={activeGroup === id ? 'yes' : ''}>
-            {name}
-          </CardText>
-          <CardText type="body1" gutterBottom active={activeGroup === id ? 'yes' : ''}>
-            Quizes activos: <b>{activeQuizes}</b>
-          </CardText>
-        </CardContent>
-        <CardActions>
-          {activeGroup === id && (
-            <DisabledButton disabled>Seleccionado</DisabledButton>
-          )}
-          {activeGroup !== id && (
-            <Button dense color="accent" onClick={selectActiveGroup(id)}>Ver Más</Button>
-          )}
-        </CardActions>
-      </Card>
-    ))}
-    <AlignCenter>
-      <Button color="accent">
-        <AddIcon />
-        Agregar grupo
-      </Button>
-    </AlignCenter>
-  </div>
-)
+  // /** Fired on add item. Set type of modal and then open modal */
+  onAdd = () => this.setState({
+    modalType: 'create',
+  }, () => this.setState({ modalSaveOpened: true }))
+
+  // /** Fired on update item. Set type of modal, id and then open modal */
+  onEdit = id => this.setState({
+    modalType: 'update',
+    toModifyId: id,
+  }, () => this.setState({ modalSaveOpened: true }))
+
+  // /** Function fired on remove item */
+  onRemove = id => this.setState({
+    toModifyId: id,
+    modalRemoveOpened: true,
+  })
+
+  onCloseModalSave = () => {
+    this.setState({ modalSaveOpened: false })
+  }
+
+  onCloseModalRemove = () => {
+    this.setState({ modalRemoveOpened: false })
+  }
+
+  render() {
+    const {
+      theme,
+      groups,
+      selectActiveGroup,
+      activeGroup,
+    } = this.props
+    const {
+      modalSaveOpened,
+      modalType,
+      toModifyId,
+      modalRemoveOpened,
+    } = this.state
+    return (
+      <div>
+        <Typography type="display1" gutterBottom>Mis cursos</Typography>
+        {groups.map(({ id, name, activeQuizes }) => (
+          <Card key={id} active={activeGroup === id ? 'yes' : ''} theme={theme}>
+            <CardContent>
+              <ContextContainer>
+                <ContextMenu
+                  value={id}
+                  handleEdit={this.onEdit}
+                  handleRemove={this.onRemove}
+                />
+              </ContextContainer>
+              <CardText type="headline" component="h2" gutterBottom active={activeGroup === id ? 'yes' : ''}>
+                {name}
+              </CardText>
+              <CardText type="body1" gutterBottom active={activeGroup === id ? 'yes' : ''}>
+                Quizes activos: <b>{activeQuizes}</b>
+              </CardText>
+            </CardContent>
+            <CardActions>
+              {activeGroup === id && (
+                <DisabledButton disabled>Seleccionado</DisabledButton>
+              )}
+              {activeGroup !== id && (
+                <Button dense color="accent" onClick={selectActiveGroup(id)}>Ver Más</Button>
+              )}
+            </CardActions>
+          </Card>
+        ))}
+        <AlignCenter>
+          <Button color="accent" onClick={this.onAdd}>
+            <AddIcon />
+            Agregar grupo
+          </Button>
+        </AlignCenter>
+        <ModalSave
+          open={modalSaveOpened}
+          title="Grupo"
+          onRequestClose={this.onCloseModalSave}
+          modalType={modalType}
+          toUpdateId={toModifyId}
+          fields={[
+            {
+              type: 'textField',
+              id: 1,
+              name: 'name',
+              label: 'Nombre del grupo',
+              required: true,
+            },
+          ]}
+        />
+        <ModalRemove
+          open={modalRemoveOpened}
+          onRequestClose={this.onCloseModalRemove}
+          toRemoveId={toModifyId}
+        />
+      </div>
+    )
+  }
+}
 
 Groups.propTypes = {
   theme: PropTypes.object.isRequired,
