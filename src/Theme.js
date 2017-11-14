38a7826f4c5080b24069c19567c8eb7c5a2da57b
Alejandro Henkel <alehenkel17@gmail.com>
2017-11-13 19:10:21 -0600

    Change interface theme

--- src/Theme.js
+++ src/Theme.js
@@ -1,6 +1,6 @@
 import { createMuiTheme } from 'material-ui/styles'
-import teal from 'material-ui/colors/teal'
-import deepOrange from 'material-ui/colors/deepOrange'
+import deepPurple from 'material-ui/colors/deepPurple'
+import green from 'material-ui/colors/green'
 import red from 'material-ui/colors/red'
 
 export const styles = theme => ({
@@ -18,8 +18,14 @@ export const styles = theme => ({
 
 export const theme = createMuiTheme({
   palette: {
-    primary: teal,
-    secondary: deepOrange,
+    primary: deepPurple,
+    secondary: {
+      ...green,
+      A100: '#80CBC4',
+      A200: '#009688',
+      A400: '#00796B',
+      A700: '#004D40',
+    },
     error: red,
   },
 })
